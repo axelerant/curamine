3e0a1ffb5e4fddb5c6ebc631191172fe1cbb5ea4
curamine <curamine@axelerant.com>
2013-04-18 14:34:10 +0000

    removing issuechecklist

--- plugins/redmine_issue_checklist/config/routes.rb
+++
@@ -1,3 +0,0 @@
-
- match "checklist/done/:checklist_item_id" => 'issue_checklists#done'
- match "checklist/delete/:checklist_item_id" => 'issue_checklists#delete'
--- plugins/redmine_issue_checklist/lib/redmine_issue_checklist/hooks/views_issues_hook.rb
+++
@@ -1,8 +0,0 @@
-module RedmineIssueChecklist
-  module Hooks
-    class ViewsIssuesHook < Redmine::Hook::ViewListener     
-      render_on :view_issues_show_description_bottom, :partial => "issues/checklist"	
-      render_on :view_issues_form_details_bottom, :partial => "issues/checklist_form"
-    end   
-  end
-end
