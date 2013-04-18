c3245d6a875060089466c1db4e441b35bae66abd
Devaroop <devaroop@devbantu.(none)>
2013-04-18 19:23:30 +0530

    removed custom workflows plugin

--- plugins/redmine_custom_workflows/db/migrate/20120628060102_change_custom_workflows_description_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCustomWorkflowsDescriptionType < ActiveRecord::Migration
-  def self.up
-    change_column :custom_workflows, :description, :text, :null => false, :default => ""
-  end
-
-  def self.down
-    change_column :custom_workflows, :description, :string, :null => false, :default => ""
-  end
-end
