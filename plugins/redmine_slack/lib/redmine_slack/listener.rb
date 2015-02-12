93ba19ef8c562d8074356163c572495e60d03943
Devaroop <devaroop123@yahoo.co.in>
2015-02-12 19:51:39 +0530

    add redmine slack plugin

---
+++ plugins/redmine_slack/lib/redmine_slack/listener.rb
@@ -0,0 +1,171 @@
+require 'httpclient'
+
+class SlackListener < Redmine::Hook::Listener
+	def controller_issues_new_after_save(context={})
+		issue = context[:issue]
+
+		channel = channel_for_project issue.project
+		url = url_for_project issue.project
+
+		return unless channel and url
+
+		msg = "[#{escape issue.project}] #{escape issue.author} created <#{object_url issue}|#{escape issue}>"
+
+		attachment = {}
+		attachment[:text] = escape issue.description if issue.description
+		attachment[:fields] = [{
+			:title => I18n.t("field_status"),
+			:value => escape(issue.status.to_s),
+			:short => true
+		}, {
+			:title => I18n.t("field_priority"),
+			:value => escape(issue.priority.to_s),
+			:short => true
+		}, {
+			:title => I18n.t("field_assigned_to"),
+			:value => escape(issue.assigned_to.to_s),
+			:short => true
+		}]
+
+		speak msg, channel, attachment, url
+	end
+
+	def controller_issues_edit_after_save(context={})
+		issue = context[:issue]
+		journal = context[:journal]
+
+		channel = channel_for_project issue.project
+		url = url_for_project issue.project
+
+		return unless channel and url
+
+		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>"
+
+		attachment = {}
+		attachment[:text] = escape journal.notes if journal.notes
+		attachment[:fields] = journal.details.map { |d| detail_to_field d }
+
+		speak msg, channel, attachment, url
+	end
+
+	def speak(msg, channel, attachment=nil, url=nil)
+		url = Setting.plugin_redmine_slack[:slack_url] if not url
+		username = Setting.plugin_redmine_slack[:username]
+		icon = Setting.plugin_redmine_slack[:icon]
+
+		params = {
+			:text => msg
+		}
+
+		params[:username] = username if username
+		params[:channel] = channel if channel
+
+		params[:attachments] = [attachment] if attachment
+
+		if icon and not icon.empty?
+			if icon.start_with? ':'
+				params[:icon_emoji] = icon
+			else
+				params[:icon_url] = icon
+			end
+		end
+
+		client = HTTPClient.new
+		client.ssl_config.cert_store.set_default_paths
+		client.ssl_config.ssl_version = "SSLv23"
+		client.post url, {:payload => params.to_json}
+	end
+
+private
+	def escape(msg)
+		msg.to_s.gsub("&", "&amp;").gsub("<", "&lt;").gsub(">", "&gt;")
+	end
+
+	def object_url(obj)
+		Rails.application.routes.url_for(obj.event_url({:host => Setting.host_name, :protocol => Setting.protocol}))
+	end
+
+	def url_for_project(proj)
+		return nil if proj.blank?
+
+		cf = ProjectCustomField.find_by_name("Slack URL")
+
+		return [
+			(proj.custom_value_for(cf).value rescue nil),
+			(url_for_project proj.parent),
+			Setting.plugin_redmine_slack[:slack_url],
+		].find{|v| v.present?}
+	end
+
+	def channel_for_project(proj)
+		return nil if proj.blank?
+
+		cf = ProjectCustomField.find_by_name("Slack Channel")
+
+		val = [
+			(proj.custom_value_for(cf).value rescue nil),
+			(channel_for_project proj.parent),
+			Setting.plugin_redmine_slack[:channel],
+		].find{|v| v.present?}
+
+		if val.to_s.starts_with? '#'
+			val
+		else
+			nil
+		end
+	end
+
+	def detail_to_field(detail)
+		if detail.property == "cf"
+			key = CustomField.find(detail.prop_key).name rescue nil
+			title = key
+		elsif detail.property == "attachment"
+			key = "attachment"
+			title = I18n.t :label_attachment
+		else
+			key = detail.prop_key.to_s.sub("_id", "")
+			title = I18n.t "field_#{key}"
+		end
+
+		short = true
+		value = escape detail.value.to_s
+
+		case key
+		when "title", "subject", "description"
+			short = false
+		when "tracker"
+			tracker = Tracker.find(detail.value) rescue nil
+			value = escape tracker.to_s
+		when "project"
+			project = Project.find(detail.value) rescue nil
+			value = escape project.to_s
+		when "status"
+			status = IssueStatus.find(detail.value) rescue nil
+			value = escape status.to_s
+		when "priority"
+			priority = IssuePriority.find(detail.value) rescue nil
+			value = escape priority.to_s
+		when "category"
+			category = IssueCategory.find(detail.value) rescue nil
+			value = escape category.to_s
+		when "assigned_to"
+			user = User.find(detail.value) rescue nil
+			value = escape user.to_s
+		when "fixed_version"
+			version = Version.find(detail.value) rescue nil
+			value = escape version.to_s
+		when "attachment"
+			attachment = Attachment.find(detail.prop_key) rescue nil
+			value = "<#{object_url attachment}|#{escape attachment.filename}>" if attachment
+		when "parent"
+			issue = Issue.find(detail.value) rescue nil
+			value = "<#{object_url issue}|#{escape issue}>" if issue
+		end
+
+		value = "-" if value.empty?
+
+		result = { :title => title, :value => value }
+		result[:short] = true if short
+		result
+	end
+end
